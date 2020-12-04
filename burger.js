dc4174349eb59d516a0e2f153ed8a58e82862cd2
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2020-12-03 22:15:01 -0300

    incluido navbar de materialize y cambios de mierda

--- burger.js
+++ burger.js
@@ -1,13 +1,3 @@
-// Selection of HTML objects
-const burger = document.querySelector('.burger i');
-const nav = document.querySelector('.nav');
-// Defining a function
-function toggleNav() {
-burger.classList.toggle('fa-bars');
-burger.classList.toggle('fa-times');
-nav.classList.toggle('nav-active');
-}
-// Calling the function after click event occurs
-burger.addEventListener('click', function() {
-toggleNav();
-});
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
