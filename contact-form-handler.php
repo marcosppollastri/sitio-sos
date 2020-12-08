fa5081f18ca4db4bffb2ddc49fd30d5f8e492745
daninkc <41238045+daninkc@users.noreply.github.com>
2020-12-08 19:59:01 -0300

    Trial

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -17,8 +17,6 @@ $to = "dani.elen.williams@gmail.com";
 
 $headers = "From: $email_from \r\n";
 
-$headers .= "Reply-To: $visitor_email \r\n";
-
 mail($to,$email_subject,$email_body,$headers);
 
 header("Location: menu_html.html");
