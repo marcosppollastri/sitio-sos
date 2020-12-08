867bb891c2d9df6a346beb086f1b4e369b12d730
daninkc <41238045+daninkc@users.noreply.github.com>
2020-12-08 19:49:03 -0300

    PHP trials

---
+++ contact-form-handler.php
@@ -0,0 +1,26 @@
+<?php
+
+$name = $_POST['name'];
+$lastname = $_POST['lastname']
+$visitor_email = $_POST['email'];
+
+$email_from = 'daniela_williams@hotmail.com';
+
+$email_subject = "Alguien se quiere comunicar con Shark";
+
+$email_body = "Nombre: $name.\n".
+                "Apellido: $lastname.\n".
+                    "Correo: $visitor_email.\n";
+
+
+$to = "dani.elen.williams@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: menu_html.html");
+
+?>
